f896987bbbe191e23ffe00cb98e42844b528a46d
Siphai PHOL <100112002+SIPHAI@users.noreply.github.com>
2022-07-15 17:15:26 +0700

    Add files via upload

---
+++ Q16 Python program to reverse an integer number.py
@@ -0,0 +1,10 @@
+number = int (input('Type a integer number:') )
+
+# print(number)
+reverse = 0
+
+while number!=0:
+    remainder = number % 10
+    reverse = reverse*10 + remainder
+    number = number//10
+print('Reverse integer number:', reverse)
