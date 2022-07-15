5b1dcb546b4f94d5d440f293b98c22c963f0c487
Siphai PHOL <100112002+SIPHAI@users.noreply.github.com>
2022-07-15 17:15:11 +0700

    Add files via upload

---
+++ Q6 Write a Python program to check whether a string is a Palindrome or not .py
@@ -0,0 +1,22 @@
+# num = 2002
+# reversed_num = 0
+# while num != 0:
+#     digit = num%10
+#     reversed_num = reversed_num * 10 +digit 
+#     num //= 10
+# # print("The reversed number is : " + str(reversed_num))
+# if reversed_num == num:
+#     print("Yes")
+    
+number = input("Type Number: ")
+reversed = number[::-1]
+# print(last_index)
+if reversed == number:
+    print(" Yessssssssssssss")
+else:
+    if reversed !=number:
+        print("Noooooooooooo")
+
+    
+
+    
