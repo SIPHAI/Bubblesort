035c7f46cf14d88e0809525c9d49162b96cdeb33
Siphai PHOL <100112002+SIPHAI@users.noreply.github.com>
2022-07-15 17:14:55 +0700

    Add files via upload

---
+++ Q2  Without using a list .py
@@ -0,0 +1,13 @@
+# list = [22, 4, 3, 2, 9, 4, 8, 5, 7, 6, 9]
+# newlist = []
+# while list:
+#     min = list[0]
+#     for i in list:
+#             if min <i:
+#                 min = i
+#     newlist.insert(0,min)
+#     # newlist.append(min)
+#     list.remove(min)
+# print(newlist)
+num = 123456
+print(str(num)[::-1])
