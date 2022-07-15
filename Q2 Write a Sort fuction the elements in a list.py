8070f82bb4b6b4fd9a77cb683b1a643c588e92bf
Siphai PHOL <100112002+SIPHAI@users.noreply.github.com>
2022-07-15 17:14:37 +0700

    Add files via upload

---
+++ Q2 Write a Sort fuction the elements in a list.py
@@ -0,0 +1,11 @@
+# This simple Sort fucntion
+List = [2,4,6,7,5,8,9,12,32,54,43,23]
+List.sort()
+print(List)
+print("\t")
+
+# This is for Reverse the Sort
+
+List = [2,4,6,7,5,8,9,12,32,54,43,23]
+List.sort(reverse = True)
+print(list)
